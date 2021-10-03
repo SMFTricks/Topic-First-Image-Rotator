608de1a4d331ab24bd5234d6987702a4fec5f9e8
Diego Andrés <diegoandres_cortes@outlook.com>
2021-10-02 23:25:21 -0500

    add spanish translation
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/FirstTopicImage/.spanish_es.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package Firs Topic Image Rotator
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2021, SMF Tricks
+ * @license MIT
+ */
+
+$txt['firsttopicimage'] = 'First Topic Image';
+$txt['firsttopicimage_enable_index'] = 'Activar las imágenes en el índice del foro';
+$txt['firsttopicimage_enable_index_desc'] = 'Por defecto solamente aparece en la lista de temas (messageindex).';
+$txt['firstopicimage_selectboards'] = 'Selecciona los foros de los que obtendrá las imágenes';
+$txt['firstopicimage_selectboards_desc'] = 'En SMF 2.0.x, separa tus foros con comas, ¡Sin espacios!';
+$txt['firsttopicimage_board_only'] = 'Mostrar solamente imágenes del foro activo';
+$txt['firsttopicimage_board_only_desc'] = 'Cuando el usuario se encuentre en ?board, solamente mostrará imágenes de temas que estén en ese foro. Solamente si el foro está en la selección.';
+$txt['firstopicimage_include_attachments'] = 'Incluir imágenes adjuntas insertadas en temas';
+$txt['firstopicimage_limit'] = 'Limite de imágenes a mostra';
+$txt['firstopicimage_limit_desc'] = 'Por defecto es 10';
+$txt['firstopicimage_width'] = 'Ancho de las imágenes';
+$txt['firstopicimage_height'] = 'Alto de las imágenes';
+$txt['firstopicimage_slides_toshow'] = 'Cantidad de imágenes a mostrar por página (slide)';
+$txt['firstopicimage_slides_toshow_desc'] = 'Por defecto es 5. <span class="smalltext">Modo Centrado se ve mejor con números impares.</span>';
+$txt['firstopicimage_slides_toscroll'] = 'Cuantas imágenes se pasan al desplazar';
+$txt['firstopicimage_slides_toscroll_desc'] = 'Por defecto es 1';
+$txt['firstopicimage_centermode'] = 'Activar Modo Centrado';
+$txt['firstopicimage_slides_autoplay'] = 'Autoplay';
+$txt['firstopicimage_slides_speed'] = 'Velocidad en milisegundos';
+$txt['firstopicimage_slides_speed_desc'] = 'Por defecto es 1500';
